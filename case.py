cb49630d5c6188d7bb082cf8a16c0ad1f481da40
balaban42 <thebalmiha42@gmail.com>
2018-11-06 02:45:45 +0700

    Commit case

--- case.py
+++ case.py
@@ -3,12 +3,25 @@ Developer:
 Balabanov M.A.
 
 """
-import ru_local
-text = input('{},'.format(ru_local, WRITE))
 
-print ('Предложений:',count_sentens)
-print ('Слов:',count_words)
-print ('Слогов:',count_syllables)
-print ('Средняя длина предложения в словах:',ASL)
-print ('Средняя длина слова в слогах:',ASW)
-print ('Индекс удобочитаемости Флеша:',FRE)
+
+text = input()
+
+count_sentences = text.count('.')
+count_words = len(text.split())
+count_syllables = 0
+for i in text:
+    letter = i.lower()
+    if letter == "а" or letter == "о" or letter == "и" or letter == "е" or letter == "ё" or letter == "э" or \
+            letter == "ы" or letter == "у" or letter == "ю" or letter == "я":
+        count_syllables += 1
+ASL = count_words / count_sentences
+ASW = count_syllables / count_words
+FRE = 206.835 - (1.3 * ASL) - (60.1 * ASW)
+
+print(count_sentences)
+print(count_words)
+print(count_syllables)
+print(ASL)
+print(ASW)
+print(FRE)
