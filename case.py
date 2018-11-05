f8054a173b0f0adb31aaa54cb12cc939f5f67f12
balaban42 <thebalmiha42@gmail.com>
2018-11-06 02:18:25 +0700

    Commit case

---
+++ case.py
@@ -0,0 +1,14 @@
+"""Case-study #4 Text analysis
+Developer:
+Balabanov M.A.
+
+"""
+import ru_local
+text = input('{},'.format(ru_local, WRITE))
+
+print ('Предложений:',count_sentens)
+print ('Слов:',count_words)
+print ('Слогов:',count_syllables)
+print ('Средняя длина предложения в словах:',ASL)
+print ('Средняя длина слова в слогах:',ASW)
+print ('Индекс удобочитаемости Флеша:',FRE)
